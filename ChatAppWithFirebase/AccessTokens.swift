678dec1d2636a9ac974fcd9676d3266aabc4dbb2
清藤凌真 <liyomaxxde@gmail.com>
2023-02-19 12:10:03 +0900

    試しにアップ

---
+++ ChatAppWithFirebase/AccessTokens.swift
@@ -0,0 +1,15 @@
+//
+//  AccessTokens.swift
+//  ChatAppWithFirebase
+//
+//  Created by 清藤凌真 on 2023/02/18.
+//
+
+import Foundation
+
+final public class AccessToken {
+    private init() {}
+    public static let shared = AccessToken()
+    
+    let newsApiKey = ""
+}
